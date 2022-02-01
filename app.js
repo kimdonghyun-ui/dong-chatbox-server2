bf4e01190121f0b67a6573ad4a755ee1f2bde034
kimdonghyun-ui <bbodaekdh@gmail.com>
2022-02-01 17:25:20 +0900

    test

--- app.js
+++ app.js
@@ -12,7 +12,8 @@ const server = http.createServer(app);
 
 const io = socketIo(server, {
   cors: {
-    origin: "http://localhost:3000",
+    // origin: "http://localhost:3000",
+    origin: "https://dong-chatbox-client2.herokuapp.com/",
     methods: ["GET", "POST"]
   }
 });
